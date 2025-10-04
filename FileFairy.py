ba38cbb67029fe383e8423bb32f6cbacb92452e1
Shalini056 <mshals2006@gmail.com>
2025-10-04 13:52:48 +0530

    Update and rename FileManager.py to FileFairy.py
    added show log feature and handling duplicate file

--- FileFairy.py
+++ FileFairy.py
@@ -26,6 +26,13 @@ def load_json(file):
             return json.load(f)
     return {}
 def handle_duplicates(path):
+    fn,ext = os.path.splitext(path)
+    counter = 1
+    new_path = path
+    while os.path.exists(new_path):
+        new_path = f"{fn}({counter}){ext}"
+        counter += 1
+    return new_path
 
   
 def organize_by_type(folder_path):
@@ -33,7 +40,7 @@ def organize_by_type(folder_path):
     undo = {}
 
     log["timestamp"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
+# try to keep history of these logs with time for future or time thookiru
     for file_name in os.listdir(folder_path):
         file_path = os.path.join(folder_path,file_name)
 
@@ -75,3 +82,16 @@ def organize_by_type(folder_path):
 
     save_json(log,LOG_FILE)
     save_json(undo,UNDO_FILE)
+
+
+def show_log_report():
+    log = load_json(LOG_FILE)
+    if not log:
+        print("No logs available")
+        return
+    print("\n📜 File Organizer Log:")
+    for file,paths in log.items():
+        if isinstance(paths,dict):
+            print(f" - {file}: {paths['from']} → {paths['to']}")
+        else:
+            print(f" -{file}: {paths}")
