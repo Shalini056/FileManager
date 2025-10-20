934af78d0b5ed371e228d3e3d24209d6b5aedce7
Shalini056 <mshals2006@gmail.com>
2025-10-20 16:17:31 +0530

    added  menu

--- FileFairy.py
+++ FileFairy.py
@@ -48,6 +48,7 @@ def organize_by_type(folder_path):
     undo = {}
 
     log["timestamp"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    log["mode"] = "type"
 # try to keep history of these logs with time for future or time thookiru
     for file_name in os.listdir(folder_path):
         file_path = os.path.join(folder_path,file_name)
@@ -91,6 +92,43 @@ def organize_by_type(folder_path):
     save_json(log,LOG_FILE)
     save_json(undo,UNDO_FILE)
 
+def organize_by_date(folder_path):
+    log = {}
+    undo = {}
+
+    log["timestamp"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    log["mode"] = "date"
+
+    for file_name in os.listdir(folder_path):
+        file_path = os.path.join(folder_path,file_name)
+        if os.path.isfile(file_path):
+            timestamp = os.path.getmtime(file_path)
+            date = datetime.datetime.fromtimestamp(timestamp)
+            year = str(date.year)
+            month = date.strftime("%B")
+
+            target_dir = os.path.join(folder_path,year,month)
+            os.makedirs(target_dir,exist_ok=True)
+
+            new_path = os.path.join(target_dir,file_name)
+            shutil.move(file_path,new_path)
+
+            log[file_name] = {"from": file_path,"to": new_path}
+            undo[new_path] = file_path
+            
+    save_json(log,LOG_FILE)
+    save_json(undo,UNDO_FILE)
+    print("✅ Organized by date (Year/Month).")
+def organize_by_size(folder_path):
+    log = {}
+    undo = {}
+    
+    log["timestamp"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    log["mode"] = "size"
+    
+
+
+
 def undo_last_operation():
     undo = load_json(UNDO_FILE)
     if not undo:
@@ -128,3 +166,39 @@ def show_log_report():
             print(f" - {file}: {paths['from']} → {paths['to']}")
         else:
             print(f" -{file}: {paths}")
+def menu():
+    while True:
+        print("\n Advanced File Organizer")
+        print("1. Organize by File Type")
+        print("2. Organize by Date")
+        print("3. Organize by Size")
+        print("4. Undo Last Operation")
+        print("5. Show Log Report")
+        print("6. Exit")
+
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            path = input("Enter folder path: ")
+            organize_by_type(path)
+        elif choice == "2":
+            path = input("Enter folder path: ")
+            organize_by_date(path)
+        elif choice == "3":
+            path = input("Enter folder path: ")
+            organize_by_size(path)
+        elif choice == "4":
+            undo_last_operation()
+        elif choice == "5":
+            show_log()
+        elif choice == "6":
+            print(" Exiting Advanced File Organizer. Goodbye!")
+            break
+        else:
+            print("Invalid choice. Try again.")
+
+# ------------------------------
+# Run Program
+# ------------------------------
+if __name__ == "__main__":
+    menu()
